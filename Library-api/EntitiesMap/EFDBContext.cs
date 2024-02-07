72537eea6fa4130dbc51dbb64b47843a46b65f91
ehsan1731 <ehsans2vt@gmail.com>
2024-02-07 10:22:30 +0330

    Migration created

---
+++ Library-api/EntitiesMap/EFDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Library_api.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace Library_api.EntitiesMap
+{
+    public class EFDBContext : DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }  
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=LibraryDB2;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+    }
+
+   
+}
---
+++ Migration/202402062140_AddTables.cs
@@ -0,0 +1,50 @@
+﻿using FluentMigrator;
+using FluentMigrator.Expressions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibMigration
+{
+
+
+    [Migration(202402062140)]
+    public class _202402062140_AddTables : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Author")
+                  .WithColumn("Id").AsInt32().Identity().PrimaryKey()
+                  .WithColumn("Name").AsString(50).NotNullable();
+
+            Create.Table("Book")
+                 .WithColumn("Id").AsInt32().Identity().PrimaryKey()
+                 .WithColumn("Title").AsString(50).NotNullable()
+                 .WithColumn("AuthorID").AsInt32().ForeignKey("FK_Book_Author", "Author", "Id")
+                 .WithColumn("Count").AsInt32();
+
+            Create.Table("Category")
+                .WithColumn("Id").AsInt32().Identity().PrimaryKey()
+                .WithColumn("BookId").AsInt32().ForeignKey("FK_Category_Book", "Book", "Id")
+                .WithColumn("Name").AsString(50).NotNullable();
+
+            Create.Table("User")
+                  .WithColumn("Id").AsInt32().Identity().PrimaryKey()
+                  .WithColumn("Name").AsString(50).NotNullable()
+                  .WithColumn("BookCount").AsInt32().NotNullable();
+
+
+        }
+
+        public override void Down()
+        {
+            Delete.Table("User");
+            Delete.Table("category");
+            Delete.Table("Book");
+            Delete.Table("Author");
+
+        }
+    }
+}
